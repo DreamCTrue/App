4f84280d145c9cd7b6541dc2f6f5dfb7e60f4771
yehchitsai <yehchitsai@gmail.com>
2014-09-03 12:20:45 +0800

    1.在登錄畫面判斷目前網路連線方式，作為是否允許登入的依據

--- www/js/login.js
+++ www/js/login.js
@@ -1,32 +1,34 @@
-var NASURL = "http://163.15.192.185/career/index.php/rest_auth";
-
-	function onLoad() { 
-	   //註冊裝置備妥處理事件
-	   document.addEventListener("deviceready", onDeviceReady, false);
-	 }
-
-	 function onDeviceReady() {
-	   checkConnection();
-	 } 
-	  
 	  //檢查網路
-	 function checkConnection() {
-		var networkState = navigator.network.connection.type;
-		 
-	   //定義網路名稱類型
-		var states = {};
-		states[Connection.UNKNOWN]  = '未知的網路型態';
-		states[Connection.ETHERNET] = '乙太網路';
-		states[Connection.WIFI]     = 'WiFi';
-		states[Connection.CELL_2G]  = '行動網路-2G';
-		states[Connection.CELL_3G]  = '行動網路-3G';
-		states[Connection.CELL_4G]  = '行動網路-4G';
-		states[Connection.NONE]     = '無網路';
-		document.getElementById('status').innerHTML = states[networkState];
-	 }
+function checkConnection() {
+	var networkState = navigator.network.connection.type;
+	 
+	//定義網路名稱類型
+	var states = {};
+	states[Connection.UNKNOWN]  = '未知的網路型態';
+	states[Connection.ETHERNET] = '乙太網路';
+	states[Connection.WIFI]     = 'WiFi';
+	states[Connection.CELL_2G]  = '行動網路-2G';
+	states[Connection.CELL_3G]  = '行動網路-3G';
+	states[Connection.CELL_4G]  = '行動網路-4G';
+	states[Connection.NONE]     = '無網路';
+	if(states[networkState]=='無網路')
+		$('#login').attr('disabled',true);
+	else
+		$('#login').attr('disabled',false);
 
+//	alert('Connection type: ' + states[networkState]);
+	$('#status').append("network status: " + states[networkState]);
+}
+document.addEventListener("deviceready", onDeviceReady, false);
+    function onDeviceReady() {
+//		alert('device ready...');
+        checkConnection();
+    }
 $(document).ready(function(){
-	var qry_str='/student/format/json';
+	var WEBURL = "https://163.15.192.185/career/index.php/rest_auth/student/format/json";
+//	alert(WEBURL);
+//	checkConnection();
+//	alert("after connect");
 	if(localStorage.getItem('re_account')=='on'){
 		$("#username").val(localStorage.getItem('name'));
 		$("#psw").val(localStorage.getItem('password'));
@@ -36,39 +38,32 @@ $(document).ready(function(){
 			$("#username").val('');
 			$("#psw").val('');
 		}
-	$(document).ready(function(){	
 	
-		$("#login").click(function(){	
-			
-			if($("#username").val() == "" || $("#psw").val() == "" ){
-				alert( "請輸入完整資料!!");
-			} else {
-				window.sessionStorage["id"] = $("#username").val();
-				$.ajax({
-					url: NASURL+qry_str,
-					type: "POST",	
-					cache: false,
-					crossDomain: true,
-					success: onSuccess,
-					error: onError,			
-					data: {
-						user_id:$("#username").val(),
-						password:$("#psw").val()
-					},
-					contentType: "application/x-www-form-urlencoded; charset=utf-8",
-					dataType: "html"
-				});				
-
-			}
-		});
+	$("#login").click(function(){	
 		
+		if($("#username").val() == "" || $("#psw").val() == "" ){
+			alert( "請輸入完整資料!!");
+		} else {
+			window.sessionStorage["id"] = $("#username").val();
+			$.ajax({
+				url: WEBURL,
+				type: "POST",	
+				cache: false,
+				crossDomain: true,
+				success: onSuccess,
+				error: onError,			
+				data: {
+					user_id:$("#username").val(),
+					password:$("#psw").val()
+				},
+				contentType: "application/x-www-form-urlencoded; charset=utf-8",
+				dataType: "html"
+			});				
 
-			//http://127.0.0.1/nas/index.php/RESTAPI/auth/format/json
+		}
 	});
 });
-function href_load(url) { //解決無法直接使用href屬性的問題
-	location.href= url;
-}
+
 function onError(data, status)
 {
 	alert(data);
