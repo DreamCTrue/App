e1c5ae9ec9806fc2e48c816bec67bdcb565fb7a6
yehchitsai <yehchitsai@gmail.com>
2014-09-03 10:59:50 +0800

    remove platforms

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD
-C:\Users\KHUSC\careermobile\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-C:\Users\KHUSC\careermobile\platforms\android\CordovaLib\AndroidManifest.xml \
-=======
-C:\Users\showshow\Career\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-C:\Users\showshow\Career\platforms\android\CordovaLib\AndroidManifest.xml \
->>>>>>> 0d1f5f0b900a9bbf4c3f38d8d8a59a6dd4204324
